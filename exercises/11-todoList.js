609ffea7f0176697eaed4724e1835465227e74be
Dean Mark <deansy0718@gmail.com>
2025-06-15 14:22:45 +0800

    TO DO LIST 1 DONE!

--- exercises/11-todoList.js
+++ exercises/11-todoList.js
@@ -1,11 +1,14 @@
 //CODE OF TO DO LIST 1
-array1 = ['make sandwich'];
+array1 = [];
 function todoList1() {
 const todolist1Input = document.querySelector('.todolist1Input');
 const gettodolist1Input = todolist1Input.value;
 
-if (gettodolist1Input) {
+if (!gettodolist1Input) {
+    alert('Input some text!') //if the value is empty, this will pop up.
+} else {
     array1.push(gettodolist1Input)
+    console.log(array1);
 }
-console.log(array1);
+todolist1Input.value = ''; //reset the input box
 }
